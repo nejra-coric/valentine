ed6ae9b9ff8dd32901d58f92cad57c3da039b5a2
nejra-coric <ncoric16@gmail.com>
2026-02-01 10:40:56 +0100

    Add enablement to configure-pages

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   output: "export",
   basePath: process.env.BASE_PATH ?? "",
   assetPrefix: process.env.BASE_PATH ? `${process.env.BASE_PATH}/` : "",
+  images: { unoptimized: true },
 };
 
 export default nextConfig;
